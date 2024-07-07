a98f5aaacb5a72da091ba90b55db581cdfb15f39
Adam Tretera <adam.tretera@heureka.cz>
2024-07-07 13:33:21 +0200

    fix build

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,20 +1,20 @@
-export function Card({title, description, image}) {
+export function Card({title, description, image}: {title:string, description:string, image:string}) {
     return (
 
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-        <img class="w-full"src={image} alt={title}/>
-            <div class="px-6 py-4">
-                <div class="font-bold text-xl mb-2">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img className="w-full"src={image} alt={title}/>
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">
                     <h2>{title}</h2>
                 </div>
-                <p class="text-gray-700 text-base">
+                <p className="text-gray-700 text-base">
                   {description}
                 </p>
             </div>
-            <div class="px-6 pt-4 pb-2">
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+            <div className="px-6 pt-4 pb-2">
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
             </div>
     </div>
 
